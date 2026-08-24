02143b075afab37d9113a489ccc40c0e18e45263
azijulhakimbd <azijulhakim.sherpur@gmail.com>
2026-08-24 15:02:46 +0600

    feat:google analytics tad add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Hind_Siliguri, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -99,6 +100,22 @@ export default async function RootLayout({
       className={`${hindSiliguri.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XTJHCF0JX3"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-XTJHCF0JX3');
+          `}
+        </Script>
+
         <AuthSessionProvider>
           <ThemeProvider>
             <Navbar session={session} />
